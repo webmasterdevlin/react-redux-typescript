22a8660fa3a3e502f7dce9f36b254913cfb1d922
Devlin <webmasterdevlin@gmail.com>
2020-01-23 12:18:39 +0100

    adjusted validation

--- src/heroes/components/HeroForm.tsx
+++ src/heroes/components/HeroForm.tsx
@@ -3,8 +3,8 @@ import Button from "react-bootstrap/Button";
 import { addHero } from "../hero-actions";
 import { useDispatch } from "react-redux";
 import { IHeroModel } from "../hero-types";
-import {Dispatch} from "redux";
-import {Formik, Form, Field, ErrorMessage, useField} from "formik";
+import { Dispatch } from "redux";
+import { Formik, Form, Field, ErrorMessage, useField } from "formik";
 import * as Yup from "yup";
 
 /* Using Formik */
@@ -19,51 +19,55 @@ const HeroForm: React.FC = () => {
   } as IHeroModel);
 
   const validationSchema = Yup.object({
-      firstName: Yup.string().required('required'),
-      lastName: Yup.string().required('required'),
-      house: Yup.string().required('required'),
-      knownAs: Yup.string().required('required'),
+    firstName: Yup.string()
+      .label("First Name")
+      .min(2)
+      .required(),
+    lastName: Yup.string()
+      .label("Last Name")
+      .min(2)
+      .required(),
+    house: Yup.string().required("required"),
+    knownAs: Yup.string().required("required")
   });
 
   return (
-      <Formik
-          initialValues={newHero}
-          validationSchema={validationSchema}
-          onSubmit={(values, actions) => {
-              dispatch(addHero(values));
-              actions.resetForm();
-          }}
-      >
-        {formikProps => (
-            <Form>
-                <Field name={'firstName'} />
-                <ErrorMessage name="firstName" component="div" />
-                <Field name={'lastName'}   />
-                <ErrorMessage name="lastName" component="div" />
-                <Field name={'house'} />
-                <ErrorMessage name="house" component="div" />
-                <Field name={'knownAs'} />
-                <ErrorMessage name="knownAs" component="div" />
-                <Button type="submit" >
-                    Send
-                </Button>
-            </Form>
-        )}
-      </Formik>
+    <Formik
+      initialValues={newHero}
+      validationSchema={validationSchema}
+      onSubmit={(values, actions) => {
+        dispatch(addHero(values));
+        actions.resetForm();
+      }}
+    >
+      {formikProps => (
+        <Form>
+          <Field name={"firstName"} />
+          <ErrorMessage name="firstName" component="div" />
+          <Field name={"lastName"} />
+          <ErrorMessage name="lastName" component="div" />
+          <Field name={"house"} />
+          <ErrorMessage name="house" component="div" />
+          <Field name={"knownAs"} />
+          <ErrorMessage name="knownAs" component="div" />
+          <Button type="submit">Send</Button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
 export default HeroForm;
 
 /* abstraction of the repeated Field and ErrorMessage above */
-const CustomField = ({...props }: any) => {
-    const [field, meta] = useField(props);
-    return (
-        <>
-            <input className="text-input" {...field} {...props} />
-            {meta.touched && meta.error ? (
-                <div className="error">{meta.error}</div>
-            ) : null}
-        </>
-    );
+const CustomField = ({ ...props }: any) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
 };
