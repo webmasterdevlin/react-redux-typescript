39359983ddca10f36849985be9fb3c48213fe8ab
Devlin D <webmasterdevlin>
2020-10-06 01:13:17 +0200

    updated

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,8 +1,5 @@
 import React, { Fragment, lazy, Suspense } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
-import Heroes from './features/heroes/pages/Heroes';
-import Villains from './features/villains/pages/Villains';
-import AntiHeroes from './features/anti-heroes/pages/AntiHeroes';
 
 type Routes = {
   exact?: boolean;
@@ -43,19 +40,6 @@ export const renderRoutes = (routes: Routes = []): JSX.Element => (
     </Switch>
   </Suspense>
 );
-
-// const Routes = () => (
-//   <Switch>
-//     <Route exact path={'/'} component={Heroes} />
-//     <Route path={'/saga'} component={Heroes} />
-//     <Route path={'/thunk'} component={Villains} />
-//     <Route path={'/toolkit'} component={AntiHeroes} />
-//
-//     <Redirect exact from={'*'} to={'/saga'} />
-//   </Switch>
-// );
-//
-// export default Routes;
 
 const routes: Routes = [
   {
