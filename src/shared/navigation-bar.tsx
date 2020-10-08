5faa3bbb657caf319c3d291f3d09a7866f6415f9
Devlin D <webmasterdevlin>
2020-10-08 08:11:55 +0200

    updated navbar

--- src/shared/navigation-bar.tsx
+++ src/shared/navigation-bar.tsx
@@ -3,22 +3,29 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import { style } from 'typestyle';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/reducers';
+import TotalOfCharacters from './TotalOfCharacters';
 
-const NavigationBar = () => (
-  <Navbar bg="dark" expand="lg" className={'mb-5'}>
-    <Nav className="mr-auto">
-      <Link className={`${navStyle} p-1 m-1`} to={'/toolkit'}>
-        Redux Toolkit
-      </Link>
-      <Link className={`${navStyle} p-1 m-1`} to={'/thunk'}>
-        Thunk
-      </Link>
-      <Link className={`${navStyle} p-1 m-1`} to={'/saga'}>
-        Saga
-      </Link>
-    </Nav>
-  </Navbar>
-);
+const NavigationBar = () => {
+  const store = useSelector((state: RootState) => state);
+  return (
+    <Navbar bg="dark" expand="lg" className={'mb-5'}>
+      <Nav className="mr-auto">
+        <Link className={`${navStyle} p-1 m-1`} to={'/toolkit'}>
+          Redux Toolkit{' '}
+          <TotalOfCharacters collection={store.antiHero.antiHeroes} />
+        </Link>
+        <Link className={`${navStyle} p-1 m-1`} to={'/thunk'}>
+          Thunk <TotalOfCharacters collection={store.villain.villains} />
+        </Link>
+        <Link className={`${navStyle} p-1 m-1`} to={'/saga'}>
+          Saga <TotalOfCharacters collection={store.hero.heroes} />
+        </Link>
+      </Nav>
+    </Navbar>
+  );
+};
 
 export default NavigationBar;
 
